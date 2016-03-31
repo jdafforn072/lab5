fc2f4e66b66280cb4e4ff5c7e7341fc3d742df23
Jessica Dafforn <jdafforn072@gmail.com>
2016-03-30 20:40:15 -0400

    initial commit

---
+++ grocerylist.js
@@ -0,0 +1,19 @@
+var myList = [{
+	item: "Coffee ",
+	price: 2.95,
+},
+{
+	item: "Milk ",
+	price: 1.99,
+},
+{
+	item: "Hot Dogs ",
+	price: 2.10,
+},
+{
+	item: "Bananas ",
+	price: .99,
+}]
+myList.forEach (function(list){
+	console.log();
+})
